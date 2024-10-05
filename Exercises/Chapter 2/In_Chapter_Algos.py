60bb1a7268750655982b7368229d3ed0a67bf654
Chris Dupre <chris@aimdyn.com>
2024-10-05 19:52:12 -0400

    Continuing to Develop

--- Exercises/Chapter 2/In_Chapter_Algos.py
+++ Exercises/Chapter 2/In_Chapter_Algos.py
@@ -7,15 +7,38 @@ def get_elt_arrays(h:float,p:np.array,
                    q:np.array,f:np.array,
                    ne:int)->Tuple[np.ndarray,np.ndarray,np.ndarray]:
     """
-
+    Takes in a set of p,q,f vectors represnenting difussion, advection
+    and forcing terms respectively. Returns a tuple of tensors representing
+    the difussion matrices, mass matrices, and forcing vectors associated to 
+    each element. 
 
     Parameters:
     -----------
+    h: float
+        Length of the element in the natural units of the problem.
+    p: np.array
+        Array of diffussion coefficients for each element.
+    q: np.array
+        Array of advection coefficients for each element. 
+    f: np.array
+        Array of forcing terms for each element. 
+    ne: int
+        Number of elements. 
 
-    
     Returns:
     --------
-
+    Kks : np.ndarray
+        Tensor where the first index determines the element, and the
+        next two correspond to the difussion matrix associated with that 
+        element. 
+    Mks: np.ndarray
+        Tensor where the first index determines the element, and the
+        next two correspond to the difussion matrix associated with that 
+        element. 
+    bks: np.ndarray
+        Tensor where the first index determines the element, and the
+        next one corresponds to the contribution due to the left and 
+        right terms respectively. 
 
     """
     #Initialize elements 
@@ -44,14 +67,49 @@ def oned_linear_FEM_Dirichlet(ne:int,
                               a:float=0,
                               b:float=1, 
                               left_val:float=0,
-                              right_val:float=0):
+                              right_val:float=0
+                              )->Tuple[np.array,np.array]:
     """
+    A 1D finite element with tent functions
+    method solver for general advection-difussion 
+    equations with Dirichlet boundrary conditions
+    in divergence form:
+
+        -d/dx(p(x)du/dx)+q(x)u(x) = f(x) on x\in (a,b)
+        u(a) = left_val
+        u(b) = right_val
+
+    Takes the number of elements you would wish to use
+    and returns the points determined and the value of 
+    the solution at those points. 
 
     Parameters:
     ----------
+    ne: int
+        Number of elements. 
+    p: np.array
+        Array of diffussion coefficients for each element.
+    q: np.array
+        Array of advection coefficients for each element. 
+    f: np.array
+        Array of forcing terms for each element. 
+    a: float
+        Left end point of the interval the function is to be 
+        defined on.
+    b: float 
+        Right end point of the interval the function is to be 
+        defined on.
+    left_val: float
+        Value the function must take on the left end point.
+    right_val: float
+        Value the function must take on the right end point. 
 
     Returns:
     --------
+    xx: np.array
+        Points in the interval the function was determined on
+    u: np.array
+        Values of the solution at the corresponding points. 
     
     """
     if b<=a:
@@ -82,8 +140,7 @@ def oned_linear_FEM_Dirichlet(ne:int,
     #Impose homogenous boundrary conditions
     A = K+M
     modified_b = b-left_val*A.getcol(0).todense().flatten()-right_val*A.getcol(-1).todense().flatten()
-    print(modified_b.shape)
-    u_int = np.linalg.solve(A[1:-1,1:-1].todense(),modified_b[1:-1])
+    u_int = sp.sparse.linalg.solve(A[1:-1,1:-1],modified_b[1:-1])
     u = np.zeros(nvtx)
     u[1:-1] = u_int
     u[0] = left_val
